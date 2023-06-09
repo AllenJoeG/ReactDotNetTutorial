4800191cd68c2f4e37fe5f99017317cb36c1952d
Joe Allen <allen.joeg@gmail.com>
2023-06-09 11:11:07 -0500

    Hunting down React bug with map, possible conflict in VSinitialized React project

--- ReactDotNet/ClientApp/src/components/RankItems.jsx
+++ ReactDotNet/ClientApp/src/components/RankItems.jsx
@@ -6,28 +6,26 @@ const RankItems = () => {
     const [items, setItems] = useState([]);
 
     //Initializing just for movie data type
-    const dataType = 1;
+    const itemType = 1;
 
     useEffect(() => {
-        fetch(`item/${dataType}`)
+        fetch(`item/${itemType}`)
             .then((results) => {
+                console.log(results);
                 return results.json();
             })
             .then(data => {
                 setItems(data);
             })
-    }, []);
+    },[])
 
     return (
-        <main>
+        <main> <h3>fix map plz</h3>
             {
-                (items != null) ?
-                    items.map((item) => <h3>{item.title}</h3>)
-                    :
-                    <div>Loading...</div>)
+                (items != null) ? items.map((item) => <h3>{item.title}</h3>) : <div><h3>Loading...</h3></div>
             }
         </main>
     )
-}
+};
 
-export default RankItems;
+export { RankItems };
