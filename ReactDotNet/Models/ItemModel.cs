379166d0ff9ed9a6c425033d9442a3a29b7584a4
Joe Allen <allen.joeg@gmail.com>
2023-06-09 09:45:07 -0500

    Initial project structure and Item Model and ItemController

---
+++ ReactDotNet/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ReactDotNet.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ImageId { get; set; }
+        public int Ranking { get; set; }
+        
+        //Items 1, Movies 2
+        public int ItemType { get; set; }
+
+    }
+}
